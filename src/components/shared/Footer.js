9f149192f515b4ea096190f8d8c7179c66c0ed2a
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-12-31 05:27:50 +0200

    - Update footer.

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <footer>
-      <div className="border-t border-black-500 shadow mt-10">
+      <div className="border-t border-black-500 border-t-1 mt-10">
         <div className="container flex flex-col-reverse items-center justify-between mx-auto md:flex-row py-6">
           <div className="text-left md:w-1/2">
             <p>Copyright &copy; 2022-2023. All Rights Reserved.</p>
           </div>
-          <div className="font-bold space-x-6 mb-3 md:w-1/2 text-right">
+          <div className="font-bold space-x-6 mb-3 md:w-1/2 md:mb-0 text-right">
             <Link to="/" title="Home" className="footer-link transition text-gray-700 hover:text-primaryBlue-600">
               Home
             </Link>
