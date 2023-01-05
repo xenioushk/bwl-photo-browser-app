1ca2e1fd79800912406c39e790f58f4ae6857bda
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2023-01-05 12:40:53 +0200

    - Fixed scroll issue.

--- src/components/pages/Page.js
+++ src/components/pages/Page.js
@@ -4,6 +4,7 @@ import React from "react"
 function Page(props) {
   useEffect(() => {
     document.title = `${props.title} | Photo Browser Application`
+    window.scroll(0, 0)
   }, [props.title])
   return <div>{props.children}</div>
 }
