3fc017dd3ef4c68e3489bea90e16ef1fae994c68
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-08-17 18:09:10 +0300

    updated the footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="border-t border-black-500 border-t-1 mt-10">
         <div className="container flex flex-col-reverse items-center justify-between mx-auto md:flex-row py-6">
           <div className="text-left md:w-1/2">
-            <p>Copyright &copy; 2022-2023. All Rights Reserved.</p>
+            <p>Copyright &copy; 2022-2024. All Rights Reserved.</p>
           </div>
           <div className="font-bold space-x-6 mb-3 md:w-1/2 md:mb-0 text-right">
             <Link to="/" title="Home" className="footer-link transition text-gray-700 hover:text-primaryBlue-600">
